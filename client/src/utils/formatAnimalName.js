bbb92f72979eaf84d580510134e69c0f9507a4cd
Jack Gannon <jackmakesart@gmail.com>
2019-11-22 17:21:59 -0700

    fixed issue with results that were equal to charLimit

--- client/src/utils/formatAnimalName.js
+++ client/src/utils/formatAnimalName.js
@@ -1,7 +1,7 @@
 export const formatAnimalName = (animalName, charLimit) => {
   let newName = animalName;
   if (animalName.length >= charLimit) {
-    newName = animalName.substr(0, charLimit);
+    newName = animalName.substr(0, charLimit - 3);
     return newName.toLowerCase() + "...";
   }
   return newName.toLowerCase();
