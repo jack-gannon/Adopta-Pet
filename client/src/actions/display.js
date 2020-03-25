4728163dcf4317f19547a0349d3f912cfbd9ea94
Jack Gannon <jackmakesart@gmail.com>
2020-03-24 22:20:26 -0700

    created alerts Redux actions

--- client/src/actions/display.js
+++ client/src/actions/display.js
@@ -1,6 +1,13 @@
+import { v4 as uuidv4 } from "uuid";
+
 import {
   SET_MODAL_OPEN,
+  SET_MODAL_COMPONENT,
+  CLEAR_MODAL_COMPONENT,
   SET_SIDEBAR_OPEN,
+  SET_ALERT_DISPLAY_OPEN,
+  ADD_ALERT,
+  REMOVE_ALERT,
   SET_DISPLAY_MOBILE
 } from "../actions/types";
 
@@ -8,8 +15,35 @@ export const setModalOpen = value => dispatch => {
   dispatch({ type: SET_MODAL_OPEN, payload: value });
 };
 
+export const openModalWithComponent = componentIdentifier => dispatch => {
+  dispatch({ type: SET_MODAL_OPEN, payload: true });
+  dispatch({ type: SET_MODAL_COMPONENT, payload: componentIdentifier });
+};
+
+export const closeModal = () => dispatch => {
+  dispatch({ type: SET_MODAL_OPEN, payload: false });
+  dispatch({ type: CLEAR_MODAL_COMPONENT });
+};
+
 export const setSidebarOpen = value => dispatch => {
   dispatch({ type: SET_SIDEBAR_OPEN, payload: value });
+};
+
+export const openAlertDisplay = () => dispatch => {
+  dispatch({ type: SET_ALERT_DISPLAY_OPEN, payload: true });
+};
+
+export const closeAlertDisplay = () => dispatch => {
+  dispatch({ type: SET_ALERT_DISPLAY_OPEN, payload: false });
+};
+
+export const addAlert = alertObject => dispatch => {
+  alertObject.id = uuidv4();
+  dispatch({ type: ADD_ALERT, payload: alertObject });
+};
+
+export const removeAlert = alertId => dispatch => {
+  dispatch({ type: REMOVE_ALERT, payload: alertId });
 };
 
 export const setDisplayMobile = value => dispatch => {
--- client/src/reducers/display.js
+++ client/src/reducers/display.js
@@ -1,12 +1,20 @@
 import {
   SET_MODAL_OPEN,
+  SET_MODAL_COMPONENT,
+  CLEAR_MODAL_COMPONENT,
   SET_SIDEBAR_OPEN,
+  SET_ALERT_DISPLAY_OPEN,
+  ADD_ALERT,
+  REMOVE_ALERT,
   SET_DISPLAY_MOBILE
 } from "../actions/types";
 
 const initialState = {
   modalIsOpen: false,
+  modalComponent: null,
   sidebarIsOpen: false,
+  alertQueue: [],
+  alertDisplayIsOpen: false,
   isMobile: false
 };
 
@@ -14,8 +22,25 @@ export default function displayReducer(state = initialState, action) {
   switch (action.type) {
     case SET_MODAL_OPEN:
       return { ...state, modalIsOpen: action.payload };
+    case SET_MODAL_COMPONENT: {
+      return { ...state, modalComponent: action.payload };
+    }
+    case CLEAR_MODAL_COMPONENT: {
+      return { ...state, modalComponent: null };
+    }
     case SET_SIDEBAR_OPEN:
       return { ...state, sidebarIsOpen: action.payload };
+    case SET_ALERT_DISPLAY_OPEN:
+      return { ...state, alertDisplayIsOpen: action.payload };
+    case ADD_ALERT:
+      return { ...state, alertQueue: [...state.alertQueue, action.payload] };
+    case REMOVE_ALERT:
+      return {
+        ...state,
+        alertQueue: [
+          ...state.alertQueue.filter(alert => alert !== action.payload)
+        ]
+      };
     case SET_DISPLAY_MOBILE:
       return { ...state, isMobile: action.payload };
     default:
